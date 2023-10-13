85c29783148e0567f8d3fdc563375f5d3e7981b5
TheMorc <r.gracik@gmail.com>
2023-10-14 00:20:03 +0200

    added font

--- quiz.py
+++ quiz.py
@@ -2,14 +2,14 @@ import pygame
 
 pygame.init()
 
-font = pygame.font.Font(None, 36)
+font = pygame.font.Font("Myriad.ttf", 36)
 projector_width, projector_height = 1024, 768
 screen = pygame.display.set_mode((projector_width, projector_height))
 bg_color = (0x18, 0x23, 0x2D)
 
 spsse_logo = pygame.image.load("spsse.png")
 
-
+audio_channel = pygame.mixer.Channel(0)
 correct_snd = pygame.mixer.Sound("correct.mp3")
 wrong_snd = pygame.mixer.Sound("wrong.mp3")
 prompt_snd = pygame.mixer.Sound("prompt.mp3")
@@ -24,24 +24,27 @@ while running:
 	
 	keys = pygame.key.get_pressed()
 	if keys[pygame.K_a]:
-		correct_snd.play()
+		audio_channel.play(correct_snd)
 		
 	if keys[pygame.K_b]:
-		correct_snd.play()
+		audio_channel.play(correct_snd)
 		
 	if keys[pygame.K_c]:
-		correct_snd.play()
+		audio_channel.play(correct_snd)
 		
 	if keys[pygame.K_x]:
-		wrong_snd.play()
+		audio_channel.play(wrong_snd)
+		
+	if keys[pygame.K_p]:
+		audio_channel.play(prompt_snd)
 		
 	if keys[pygame.K_q]:
-		question_snd.play()
+		audio_channel.play(question_snd)
 	
 	screen.fill(bg_color)
 	
 	text = font.render("Hello, Pygame!", True, (255, 255, 255))
-	screen.blit(text, (200, 200))
+	screen.blit(text, (200, 200, 50, 50))
 	
 	# Draw an image
 	screen.blit(spsse_logo, (300, 300))
